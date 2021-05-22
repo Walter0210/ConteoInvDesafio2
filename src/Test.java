173a803e37cabb50ab316c469084c18b377e857e
walter0210 <walter.ebalderrama@gmail.com>
2021-05-22 16:51:40 -0300

    hi2

--- src/Test.java
+++ src/Test.java
@@ -20,10 +20,56 @@ public class Test {
                 numeros.add(n);
                 //System.out.println(data);
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
-            catch (FileNotFoundException e) {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
     }
-}wa
+
+
+    private class MergeSort {
+        // vector auxiliar para almacenar las copias temporales
+        private int temp[];
+        private int[] v;
+
+        // ordenamiento Merge Sort
+        public void ordenar() {
+            int n = v.length;
+            temp = new int[n];
+            sort(0, n - 1);
+        }
+
+        private void sort(int izq, int der) {
+            if (izq < der) {
+                int centro = (izq + der) / 2;
+                sort(izq, centro);
+                sort(centro + 1, der);
+                merge(izq, centro, der);
+            }
+        }
+
+        private void merge(int izq, int centro, int der) {
+            for (int i = izq; i <= der; i++) {
+                temp[i] = v[i];
+            }
+
+            int i = izq, j = centro + 1, k = izq;
+            while (i <= centro && j <= der) {
+                if (temp[i] <= temp[j]) {
+                    v[k] = temp[i];
+                    i++;
+                } else {
+                    v[k] = temp[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i <= centro) {
+                v[k] = temp[i];
+                k++;
+                i++;
+            }
+        }
+    }
+}
